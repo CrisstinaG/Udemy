1e7f958682abd752eace1616d5de860f0f0654e5
Cristina <cgcid@indra.es>
2021-02-17 17:41:05 +0100

    primer commit con develop

--- typescript/typescript/app.ts
+++ typescript/typescript/app.ts
@@ -4,4 +4,4 @@ function nombreCompleto (nombre:string, apellido:string ):string{
     return nombre + '' + apellido;
 }
 let nombre = nombreCompleto ("Clark","Kent");
-console.log(nombre);
+console.log(nombre);  
